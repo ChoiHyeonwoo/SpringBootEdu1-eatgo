593441a18dac8b3e4932ac64a11b13503f3079f5
ChoiHyeonWoo <201111267@sangmyung.kr>
2020-05-18 01:38:22 +0900

    2020.05.17 / DI(Dependency Injection) / Layer 분리 지난 시간 interface -> controller domain -> domain, repository
    이제!
    Application Layer -> Restaurant, service 객체 추가

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -1,10 +1,14 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
 
     private final String name;
     private final String address;
     private Long id;
+    private List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
     public Restaurant(long id, String name, String address) {
         this.id = id;
@@ -22,6 +26,20 @@ public class Restaurant {
 
     public Long getId() {
         return id;
+    }
+
+    public List<MenuItem> getMenuItems(){
+        return menuItems;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public void setMenuItem(List<MenuItem> menuItems) {
+        for (MenuItem menuItem: menuItems) {
+            addMenuItem(menuItem);
+        }
     }
 
     public String getInformation() {
