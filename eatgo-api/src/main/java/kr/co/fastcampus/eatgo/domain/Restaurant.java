3a3daef70be7f7c02a7ad14aafc9d203f61f2f70
ChoiHyeonWoo <201111267@sangmyung.kr>
2020-05-17 19:47:30 +0900

    2020.05.17 / RestAPI를 통한 List형태 목록 및 상세 작성. /
     RestAPI
    여러개의 Front-end를 지원
    
    Representional state Transfer (상태)
    Resource 상태를 표현
    CRUD
    Create -> post
    Read -> get
    update -> put/patch
    delete -> delete
    
    리소스는 두개로 구분
    Collection
        읽기와 생성이 가능
        목록을 얻음
        새로운 리소스 생성
    ex) http://host/restaurants
    
    Member
        각 개별 리소스를 얻음
        각 개별 리소스 수정
        각 개별 리소스 삭제
    ex) http://host/restaurant/1
    
    JSON 형태로 표현
    
    목록
    GET     /restaurants
    상세
    GET     /restaurant/{id}
    추가
    POST    /restaurants
    수정
    PATCH   /restaurant/{id}
    삭제
    PATCH   /restaurant/{id}
    
    cf) List.stream -> 자바8 API 추가
            Restaurant restaurant = restaurants.stream() // java 8 부터 추가
                    .filter(r -> r.getId().equals(id))  // id가 같은 것만 걸러서
                    .findFirst() // 맨 처음 것만 가져옴. (.get()을 통해 가져오기도 함.)
                    .orElse(null); (예외처리 : id가 없을 경우 null 처리)

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -4,8 +4,10 @@ public class Restaurant {
 
     private final String name;
     private final String address;
+    private Long id;
 
-    public Restaurant(String name, String address) {
+    public Restaurant(long id, String name, String address) {
+        this.id = id;
         this.name = name;
         this.address = address;
     }
@@ -16,6 +18,10 @@ public class Restaurant {
 
     public String getAddress() {
         return address;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getInformation() {
