dc37b3d77b467544676e640555ce4131ef2ab05b
ChoiHyeonWoo <201111267@sangmyung.kr>
2020-06-23 22:39:17 +0900

    24강 가게목록 필터링 지역을 통한 분류 (Region) 음식 종류에 대한 분류 (Category)
    -> 도메인 모델로 관리
    
    admin-api
     GET /regions
     POST /regions
     GET /categories
     POST /categories
    customer-api
     GET /regions
     GET /categories
    
     findAllByAddressContaining -> 문자열을 포함하는 경우(서울시 구로구 -> 서울!)
     findAllByAddressStartingWith -> '서울' 로 시작
    h2 console
    
    localhost:8080/h2-console

---
+++ eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/RegionTests.java
@@ -0,0 +1,16 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RegionTests {
+
+    @Test
+    public void creation(){
+        Region region = Region.builder().name("서울").build();
+
+        assertThat(region.getName(), is("서울"));
+    }
+}
