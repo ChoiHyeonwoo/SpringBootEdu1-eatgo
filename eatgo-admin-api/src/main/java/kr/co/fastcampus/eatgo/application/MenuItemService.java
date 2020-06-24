21f36341eaf4e59ddff00d649a74618735285fb2
ChoiHyeonWoo <201111267@sangmyung.kr>
2020-06-24 22:47:52 +0900

    25사용자관리

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service //PR 테스트
 public class MenuItemService {
 
     private MenuItemRepository menuItemRepository;
