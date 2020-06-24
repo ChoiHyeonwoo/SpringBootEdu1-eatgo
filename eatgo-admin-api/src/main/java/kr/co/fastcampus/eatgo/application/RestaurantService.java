db2610f69951c9c0498830e42900b71a7fb64a07
ChoiHyeonWoo <201111267@sangmyung.kr>
2020-06-24 22:34:45 +0900

    SpringBootEdu1-eatgo-1 Github issue Test && fastcampus 24th
    24강 가게목록 필터링
    
    CrudRepository
    findAllBy~~~Containing
     like조건 검색 가능
    
    https://arahansa.github.io/docs_spring/jpa.html
    ctrl + f -> containing 으로 검색시 [표 4. 메소드 이름 안에서 지원되는 키워드들]
    
    findAllByAddressContainingAndCategoryId
        -> where address like ~ and categoryId = ''

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/RestaurantService.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/RestaurantService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@Service //testtest
+@Service
 public class RestaurantService {
 
     private RestaurantRepository restaurantRepository;
